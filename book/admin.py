675a6f545a069aa2af23484d8bf6fb8294411f21
c62 <nsw9211@gmail.com>
2016-06-10 16:55:36 +0900

    init commit

---
+++ book/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from book import models
+
+admin.site.register(models.Region)
+admin.site.register(models.Hashtag)
+admin.site.register(models.Menu)
+admin.site.register(models.Taste_user)
+admin.site.register(models.Restaurant)
+admin.site.register(models.Record)
+admin.site.register(models.Satisfaction)
+admin.site.register(models.Group)
+admin.site.register(models.Image)
